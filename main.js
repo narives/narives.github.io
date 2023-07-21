34396de3107367fc9903bf5566d2616f18f84cd3
Nari <nuyhank@gmail.com>
2023-07-22 05:05:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-// 스크롤 시 메뉴바 고정
-window.addEventListener('scroll', function () {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 0) {
-        nav.classList.add('fixed');
-    } else {
-        nav.classList.remove('fixed');
-    }
-});
-
-// 메뉴 클릭 시 부드러운 스크롤링
-const menuLinks = document.querySelectorAll('nav ul li a');
-menuLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-            
-        // 메뉴바의 높이만큼 오프셋을 주어서 컨텐츠가 메뉴바 아래에서 시작되도록 조정
-        const navHeight = document.querySelector('nav').offsetHeight;
-        const targetPosition = target.getBoundingClientRect().top;
-        const offsetPosition = targetPosition - navHeight;
-
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
+$(document).ready(function() {
+    $("#bthome").on('click', function () {
+      // html, body 요소 모두에 애니메이션을 적용합니다.
+      $("html, body").animate({ scrollTop: '0' }, 500);
     });
-});
+  });
+  
+  $(document).ready(function() {
+    // 메뉴바 링크를 클릭하면 해당 섹션으로 스크롤 이동
+    $("nav a").on('click', function(event) {
+      event.preventDefault();
+      var target = $(this).attr("href");
+      var targetPosition = $(target).offset().top;
+      $('html, body').animate({ scrollTop: targetPosition }, 500);
+    });
+  });
+  
